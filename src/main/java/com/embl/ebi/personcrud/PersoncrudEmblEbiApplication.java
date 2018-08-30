f23d7f5c1429f3d8aad03d80138457c6fcc2e794
Gabriel Machado <gabriel.d.machado92@gmail.com>
2018-08-30 15:37:17 -0300

    First commit - Project created

---
+++ src/main/java/com/embl/ebi/personcrud/PersoncrudEmblEbiApplication.java
@@ -0,0 +1,12 @@
+package com.embl.ebi.personcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersoncrudEmblEbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersoncrudEmblEbiApplication.class, args);
+	}
+}
