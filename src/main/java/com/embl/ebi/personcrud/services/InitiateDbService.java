49ec1b2fe8bfb158955a37b69987b9b8ded8e4d6
Gabriel Machado <gabriel.d.machado92@gmail.com>
2018-09-02 16:59:44 -0300

    Added pagination for Person listaAll

--- src/main/java/com/embl/ebi/personcrud/services/InitiateDbService.java
+++ src/main/java/com/embl/ebi/personcrud/services/InitiateDbService.java
@@ -24,12 +24,43 @@ public class InitiateDbService {
 		
 		Person p1 = new Person();
 		
-		p1.setAge(22);
-		p1.setFavouriteColor("Blue");
-		p1.setFirstName("Gabriel");
-		p1.setLastName("Machado");
+		p1.setAge(32);
+		p1.setFavouriteColor("Green");
+		p1.setFirstName("Joao");
+		p1.setLastName("Silva");
+		
+		Person p2 = new Person();
+		
+		p2.setAge(22);
+		p2.setFavouriteColor("Blue");
+		p2.setFirstName("Gabriel");
+		p2.setLastName("Machado");
+		
+		Person p3 = new Person();
+		
+		p3.setAge(40);
+		p3.setFavouriteColor("Yellow");
+		p3.setFirstName("Bernard");
+		p3.setLastName("Cool");
+		
+		Person p4 = new Person();
+		p4.setAge(35);
+		p4.setFavouriteColor("Purple");
+		p4.setFirstName("Happy");
+		p4.setLastName("John");
+		
+		Person p5 = new Person();
+		
+		p5.setAge(50);
+		p5.setFavouriteColor("Black");
+		p5.setFirstName("Mathew");
+		p5.setLastName("Pirahy");
 		
 		personService.insert(p1);
+		personService.insert(p2);
+		personService.insert(p3);
+		personService.insert(p4);
+		personService.insert(p5);
 		
 	}
 	
