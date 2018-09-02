37e67bb574596d5adfec53ac797deeb301d171b7
Gabriel Machado <gabriel.d.machado92@gmail.com>
2018-09-02 16:04:53 -0300

    Finishing swagger implementation

--- src/main/java/com/embl/ebi/personcrud/config/SecurityConfig.java
+++ src/main/java/com/embl/ebi/personcrud/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -31,6 +32,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	public static final String[] PUBLIC_MATCHERS_GET = {
 			"/people/**"
 	};
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		 web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**",
+		"/swagger-ui.html", "/webjars/**");
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
